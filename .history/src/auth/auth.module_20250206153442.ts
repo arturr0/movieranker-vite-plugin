49939da7fc3caadbe323ce12f21e99ae3c4e982a
Artur Bednarski <01005196@pw.edu.pl>
2025-02-06 15:43:28 +0100

    tab indentation, before retro style

--- .history/src/auth/auth.module_20250206153442.ts
+++ .history/src/auth/auth.module_20250206153442.ts
@@ -8,14 +8,14 @@ import { PrismaService } from '../prisma/prisma.service';
 import 'dotenv/config'; // Loading environment variables
 
 @Module({
-	imports: [
-		JwtModule.register({
-			secret: process.env.JWT_SECRET || 'default-secret',  // Ensure JWT secret is correctly set
-			signOptions: { expiresIn: '1h' },  // JWT expiration
-		}),
-	],
-	controllers: [AuthController],
-	providers: [AuthService, PrismaService, JwtStrategy, JwtAuthGuard],
-	exports: [AuthService, JwtAuthGuard],  // Exporting JwtAuthGuard if needed outside this module
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default-secret',  // Ensure JWT secret is correctly set
+      signOptions: { expiresIn: '1h' },  // JWT expiration
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, JwtStrategy, JwtAuthGuard],
+  exports: [AuthService, JwtAuthGuard],  // Exporting JwtAuthGuard if needed outside this module
 })
 export class AuthModule {}
