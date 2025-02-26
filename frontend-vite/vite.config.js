20e73092b083c0e64fa31baf31037ec93699a34e
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-02-26 18:12:45 +0100

    Update vite.config.js

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Use vue if it's Vue
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 10000, // Set a different port, e.g., 3000 (or any available port)
     proxy: {
-      '/api': 'https://movieranker-react.onrender.com', // Proxy API requests to NestJS backend
+      '/api': 'https://movieranker-vite.onrender.com', // Proxy API requests to NestJS backend
     },
   },
 });
