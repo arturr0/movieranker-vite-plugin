fd3014bfda404e43d20d8212de3f24b99aca77df
Artur Bednarski <01005196@pw.edu.pl>
2025-02-26 16:57:43 +0100

    nest and vite connected

---
+++ frontend-vite/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Use vue if it's Vue
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // Proxy API requests to NestJS backend
+    },
+  },
+});
